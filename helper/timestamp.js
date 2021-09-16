39f2402a92fd025e073d5ea6564d4bf470e298cc
itzFlubby <leonardknapp@web.de>
2021-09-16 17:18:11 +0200

    Padding in timestamp

--- helper/timestamp.js
+++ helper/timestamp.js
@@ -1,6 +1,6 @@
 function formatTimestamp(pTimestamp){
 	const formttedTimestamp = new Date(pTimestamp);
-	return `${formttedTimestamp.getDate()}.${formttedTimestamp.getMonth() + 1}.${formttedTimestamp.getFullYear()} um ${formttedTimestamp.getHours()}:${formttedTimestamp.getMinutes()}:${formttedTimestamp.getSeconds()}`
+	return `${formttedTimestamp.getDate().toString().padStart(2, '0')}.${(formttedTimestamp.getMonth() + 1).toString().padStart(2, '0')}.${formttedTimestamp.getFullYear()} um ${formttedTimestamp.getHours().toString().padStart(2, '0')}:${formttedTimestamp.getMinutes().toString().padStart(2, '0')}:${formttedTimestamp.getSeconds().toString().padStart(2, '0')}`
 }
 
 module.exports = {
