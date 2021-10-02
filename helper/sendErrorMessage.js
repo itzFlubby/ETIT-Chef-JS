bea5d98c2c326d9e473588b798c75a1c76ac07d8
itzFlubby <leonardknapp@web.de>
2021-10-02 12:04:31 +0200

    Send error message directly to channel

--- helper/sendErrorMessage.js
+++ helper/sendErrorMessage.js
@@ -1,5 +1,17 @@
 const Discord = require("discord.js");
 const embedHelper = require("../helper/embed.js");
+const { settings } = require("../ETIT-Chef.js");
+
+function _formatUsage(pName, pList) {
+	let usage = `Ausführung:\n${settings.prefix}${pName}`;
+	let parameters = "";
+	for ( parameter in pList ) {
+		usage += ` {${pList[parameter][0]}}`;
+		parameters += `${pList[parameter][0]} :\n${pList[parameter][1].join(", ")}\n\n`;
+	}
+	
+	return usage + "\n\n" +  parameters;
+}
 
 function sendErrorMessage(pClient, pMessage, pTitle, pDescription) {
 	const embed = embedHelper.constructDefaultEmbed(pClient)
@@ -12,6 +24,17 @@ function sendErrorMessage(pClient, pMessage, pTitle, pDescription) {
 	});
 }
 
-module.exports = {
-	sendErrorMessage
+function sendErrorMessageToChannel(pClient, pChannel, pTitle, pDescription) {
+	const embed = embedHelper.constructDefaultEmbed(pClient)
+		.setColor("#CC0000")
+		.setTitle(pTitle)
+		.setDescription(pDescription);
+		
+	pChannel.send({ 
+		embeds: [ embed ], 
+	});
 }
+
+module.exports = {
+	_formatUsage, sendErrorMessage, sendErrorMessageToChannel
+};
