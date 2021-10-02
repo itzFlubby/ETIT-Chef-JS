2b9a523f19aab7d35a4ddf9ef8ec93c4060b0779
itzFlubby <leonardknapp@web.de>
2021-10-02 12:03:07 +0200

    Format date-only function

--- helper/timestamp.js
+++ helper/timestamp.js
@@ -1,8 +1,12 @@
 function formatTimestamp(pTimestamp){
 	const formttedTimestamp = new Date(pTimestamp);
-	return `${formttedTimestamp.getDate().toString().padStart(2, '0')}.${(formttedTimestamp.getMonth() + 1).toString().padStart(2, '0')}.${formttedTimestamp.getFullYear()} um ${formttedTimestamp.getHours().toString().padStart(2, '0')}:${formttedTimestamp.getMinutes().toString().padStart(2, '0')}:${formttedTimestamp.getSeconds().toString().padStart(2, '0')}`
+	return `${formttedTimestamp.getDate().toString().padStart(2, '0')}.${(formttedTimestamp.getMonth() + 1).toString().padStart(2, '0')}.${formttedTimestamp.getFullYear()} um ${formttedTimestamp.getHours().toString().padStart(2, '0')}:${formttedTimestamp.getMinutes().toString().padStart(2, '0')}:${formttedTimestamp.getSeconds().toString().padStart(2, '0')}`;
+}
+
+function formatDate(pDate){
+	return `${pDate.getDate().toString().padStart(2, '0')}.${(pDate.getMonth() + 1).toString().padStart(2, '0')}.${pDate.getFullYear()}`;
 }
 
 module.exports = {
-	formatTimestamp
+	formatTimestamp, formatDate
 };
